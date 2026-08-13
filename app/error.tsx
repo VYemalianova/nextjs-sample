2c04004474f5cdc616c50f109416d3348823766e
Viktoriia Yemelianova <v.yemelianova@mobidev.biz>
2026-08-13 21:49:52 +0300

    add error page

--- app/error.tsx
+++ app/error.tsx
@@ -1,7 +1,38 @@
 'use client';
 
+import Image from 'next/image';
+import Link from 'next/link';
+
 const Error = ({ error }: { error: Error }) => {
-  return <div>{error?.message || 'An unexpected error occurred.'}</div>;
+  const message = error?.message || 'An unexpected error occurred.';
+
+  return (
+    <div className="h-full flex items-center justify-center p-8">
+      <div className="flex items-center justify-center gap-32 max-sm:flex-col">
+        <Image
+          src="/images/error.png"
+          alt="Error illustration"
+          width={89}
+          height={89}
+          priority
+        />
+
+        <div className="flex flex-col">
+          <h2>Oops! Something went wrong...</h2>
+
+          <p className="mt-8 text-xl">{message}</p>
+
+          <p className="mt-8">
+            Please{' '}
+            <Link href="/" className="text-purple-01 underline font-bold">
+              go back to the homepage
+            </Link>{' '}
+            and try again.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Error;
